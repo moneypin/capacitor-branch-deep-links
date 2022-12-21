aa014157c62f4943036c67499d0ed07294c4dd1c
junho <junho@moneypin.biz>
2022-12-21 17:13:44 +0900

    update: delete web implementation

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import type { BranchDeepLinksPlugin } from './definitions';
 
 const BranchDeepLinks = registerPlugin<BranchDeepLinksPlugin>(
   'BranchDeepLinks',
-  {
-    web: () => import('./web').then(m => new m.BranchDeepLinksWeb()),
-  },
 );
 
 export * from './definitions';
 export { BranchDeepLinks };
+
